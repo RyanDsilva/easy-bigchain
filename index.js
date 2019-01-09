73ccc86ab6a0710fd00b51f6c7105945991cc8c8
Ryan Dsilva <ryan.dsilva.98@gmail.com>
2019-01-09 20:39:11 +0530

    Changes and Test

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+let bigchain = exports
+
 const connect = require('./src/functions/connect')
 const keypair = require('./src/functions/keypair')
 const create = require('./src/functions/create')
 const transfer = require('./src/functions/transfer')
 const search = require('./src/functions/search')
 
-module.exports = {
-  connect: connect,
-  generateKeypair: keypair,
-  createAsset: create,
-  transferAsset: transfer,
-  search: search
-}
+bigchain.connect = connect
+bigchain.generateKeypair = keypair
+bigchain.createAsset = create
+bigchain.transferAsset = transfer
+bigchain.search = search
